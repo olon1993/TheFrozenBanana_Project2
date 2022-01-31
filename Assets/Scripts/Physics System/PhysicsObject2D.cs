93955559abe7fb58f7cb07990aaa62002ac7b4c3
Scott Batey <sebateyjr@yahoo.com>
2022-01-31 13:02:35 -0500

    Moved scripts out of the development folder

--- Assets/Scripts/Physics System/PhysicsObject2D.cs
+++ Assets/Scripts/Physics System/PhysicsObject2D.cs
@@ -32,11 +32,6 @@ public class PhysicsObject2D : RaycastController
     //******************** Methods *********************\\
     //**************************************************\\
 
-    //protected override void Awake()
-    //{
-    //    base.Awake();
-    //}
-
     protected override void Start()
     {
         base.Start();
