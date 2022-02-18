bf4c5047f4394614196c8718f98c0a6295cccad9
Pixelwortel <hiemstra.wouter@gmail.com>
2022-02-18 12:34:22 +0100

    New Projectile scripts
    The new projectile interface and scripts are added. IProjectile, wProjectile, wProjectileChild.
    
    Note: the w prefix can be removed after acceptance in code review.

---
+++ Assets/Scenes/Development/Wouter/Scripts/ForReview/wProjectileChild.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheFrozenBanana;
+
+namespace TheFrozenBanana {
+
+	public class wProjectileChild : MonoBehaviour {
+
+		[SerializeField] private IProjectile projectileParent;
+		private Collider2D myCollider;
+
+		private void Awake() {
+			projectileParent = gameObject.GetComponentInParent<IProjectile>();
+			myCollider = gameObject.GetComponent<Collider2D>();
+			if (myCollider == null) {
+				Debug.LogError(gameObject.GetComponentInParent<GameObject>().name + " has no proper collider setup! Check child in Prefab!");
+			}
+ 		}
+
+		private void OnTriggerEnter2D(Collider2D col) {
+			projectileParent.TriggerCollision(col);
+		}
+	}
+}
