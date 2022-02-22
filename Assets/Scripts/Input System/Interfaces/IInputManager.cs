7da858872396efca58a2ecfe20b134a7959ef3da
richie-oates <richoates2020@gmail.com>
2022-02-22 20:17:00 +0100

    Create IInputManager.cs

---
+++ Assets/Scripts/Input System/Interfaces/IInputManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheFrozenBanana
+{
+    public interface IInputManager
+    {
+        float Horizontal { get; }
+        float Vertical { get; }
+        bool IsJump { get; }
+        bool IsJumpCancelled { get; }
+        bool IsDash { get; }
+        bool IsAttack { get; }
+    }
+}
+
