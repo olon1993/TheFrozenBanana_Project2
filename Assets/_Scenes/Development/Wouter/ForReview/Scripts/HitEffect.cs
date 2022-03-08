c70f55c8df4859d6d9343b45a5586b82073c939f
Scott Batey <sebateyjr@yahoo.com>
2022-03-07 22:43:44 -0500

    Decorated level 0 more

--- Assets/_Scenes/Development/Wouter/ForReview/Scripts/HitEffect.cs
+++ Assets/_Scenes/Development/Wouter/ForReview/Scripts/HitEffect.cs
@@ -7,26 +7,31 @@ namespace TheFrozenBanana
 {
 	public class HitEffect : MonoBehaviour 
 	{
-		[SerializeField] private float destroyTimer;
-		[SerializeField] private AudioSource heas;
-		[SerializeField] private Animator heac;
-		[SerializeField] private string heAnimationName;
+        [SerializeField] private float destroyTimer;
+        [SerializeField] private AudioSource heas;
+        [SerializeField] private Animator heac;
+        [SerializeField] private string heAnimationName;
 
-		private void OnAwake() {
-			if (destroyTimer == 0f) {
-				destroyTimer = 2f;
-			}
-			PlayHitEffect();
-		}
+        private void OnAwake()
+        {
+            if (destroyTimer == 0f)
+            {
+                destroyTimer = 2f;
+            }
+            PlayHitEffect();
+        }
 
-		private void PlayHitEffect() {
-			if (heas != null) {
-				heas.Play();
-			}
-			if (heac != null) {
-				heac.Play(heAnimationName);
-			}
-			Destroy(this.gameObject, destroyTimer);
-		}
-	}
+        private void PlayHitEffect()
+        {
+            if (heas != null)
+            {
+                heas.Play();
+            }
+            if (heac != null)
+            {
+                heac.Play(heAnimationName);
+            }
+            Destroy(this.gameObject, destroyTimer);
+        }
+    }
 }
